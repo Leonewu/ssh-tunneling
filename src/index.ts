2c9de44da7134c22b12c2c295e5853fe5d654d13
joel <joel.edwardson@whiteswandata.com>
2023-01-20 16:54:42 +0000

    expose inner SSH client and status

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { SocksClient, SocksClientOptions } from 'socks';
 import logger from './logger';
 import { getAvailablePort } from './utils';
 
-enum STATUS {
+export enum STATUS {
   INIT = 0,
   CONNECTING,
   READY,
@@ -480,6 +480,13 @@ class SshTunnel {
     }
     const targetList = this.proxyList.filter(item => id ? item.id === id : true);
     targetList.forEach(item => item.server.close());
+  }
+
+  getSSHClient(): SshClient | undefined  {
+    return this.sshClient
+  }
+  getSocksStatus(): STATUS {
+    return this.socksStatus
   }
 
 }
